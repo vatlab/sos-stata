1e1294f33370f4bd29cf72af5cea7d5b77284482
Bo Peng <ben.bog@gmail.com>
2018-12-02 17:29:33 -0600

    Add skeleton of Stata module for SoS

---
+++ README.md
@@ -0,0 +1,4 @@
+[![PyPI version](https://badge.fury.io/py/sos-stata.svg)](https://badge.fury.io/py/sos-stata)
+
+# sos-stata
+SoS extension for Stata. Please refer to the [SoS homepage](http://vatlab.github.io/SOS) for more information.
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env javascript
+#
+# Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
+# Distributed under the terms of the 3-clause BSD License.
+
+from setuptools import find_packages, setup
+
+# obtain version of SoS
+with open('src/sos_stata/_version.py') as version:
+    for line in version:
+        if line.startswith('__version__'):
+            __version__ = eval(line.split('=')[1])
+            break
+
+setup(name = "sos-stata",
+    version = __version__,
+    description = 'SoS Notebook extension for stata',
+    author = 'Bo Peng',
+    url = 'https://github.com/vatlab/sos-stata',
+    author_email = 'bpeng@mdanderson.org',
+    maintainer = 'Bo Peng',
+    maintainer_email = 'bpeng@mdanderson.org',
+    license = '3-clause BSD',
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        ],
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    install_requires=[
+          'sos>=0.9.12.0',
+          'sos-notebook>=0.9.11.6'
+      ],
+    entry_points= '''
+[sos_languages]
+stata = sos_stata.kernel:sos_stata
+'''
+)
